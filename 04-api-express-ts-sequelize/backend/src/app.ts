d5266ee632c1f89acdf1452a4dc3abdd38ed659b
Fernando Gómez <difex01@gmail.com>
2022-12-13 18:46:59 -0600

    04 - Redirect route added

--- 04-api-express-ts-sequelize/backend/src/app.ts
+++ 04-api-express-ts-sequelize/backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import router from './routes';
 
 const app = express();
@@ -7,5 +7,11 @@ const app = express();
 app.use(express.json())
 
 app.use('/', router);
+
+app.get('/music', (_req: Request, res: Response) => {
+  res.writeHead(301, {
+    Location: 'https://youtu.be/i32E7GPSwRw',
+  }).end();
+});
 
 export default app;
